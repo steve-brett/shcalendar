e44236e7fc5d5ce1e1f2cb871972d9d2ec3eac57
Steve <steve@steves-macbook-pro.local>
2018-11-14 19:34:22 +0000

    Updating styling

--- dateform_dump.php
+++ dateform_dump.php
@@ -1,54 +1,89 @@
+<?php
 
+if(isset($_POST['action']) && $_POST['action'] == 'Submit') {
+
+  if(isset($_POST['singDate']) && strlen($_POST['singDate']) > 0) {
+
+    $singDate = $_POST['singDate'];
+}
+  if(isset($_POST['singDate-day']) && $_POST['singDate-day'] > 0) {
+
+    $singDateDay = $_POST['singDate-day'];
+}
+
+
+  if(isset($singDate) && isset($singDateDay))
+  {
+    $output = $singDateDay;
+    $form_success = true;
+
+  } else {
+
+    $form_success = false;
+    $output = 'Error: no values passed';
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>title</title>
+    <title>Sacred Harp calendar calculator</title>
+    <link href="https://design-system.service.gov.uk/stylesheets/main-2beab115c64c3ab6a1bc2c3d53d93233.css" rel="stylesheet" media="all">
+    <link href="http://cdn.govstrap.io/v1/css/govstrap.min.css" rel="stylesheet">
     <!--<link rel="stylesheet" href="style.css">
     <script src="script.js"></script> -->
   </head>
   <body>
-
-  <div class="govuk-form-group">
-  <fieldset class="govuk-fieldset" aria-describedby="dob-hint" role="group">
-    <legend class="govuk-fieldset__legend govuk-fieldset__legend--xl">
-      <h1 class="govuk-fieldset__heading">
+<div class="container">
+<form method="post" action="">
+  <div class="form-group">
+  <fieldset class="fieldset" aria-describedby="singDate-hint" role="group">
+    <legend class="fieldset__legend fieldset__legend--xl">
+      <h1 class="fieldset__heading">
         What date is your singing?
       </h1>
     </legend>
-    <span id="dob-hint" class="govuk-hint">
+    <span id="singDate-hint" class="help-block">
       For example, 12 6 2018. This can be any past or future singing that
-      follows your formula. Do not use the date of a one-off exception: you can
-      set this later.
+      follows your formula.<br/>
+      Do not use the date of a one-off exception: you can set this later.
     </span>
-    <div class="govuk-date-input" id="dob">
-      <div class="govuk-date-input__item">
-        <div class="govuk-form-group">
-          <label class="govuk-label govuk-date-input__label" for="dob-day">
+    <div class="date-input form-inline" id="singDate">
+      <div class="date-input__item">
+        <div class="form-group">
+          <label class="label date-input__label" for="singDate-day">
             Day
           </label>
-          <input class="govuk-input govuk-date-input__input govuk-input--width-2" id="dob-day" name="dob-day" type="number" pattern="[0-9]*">
+          <input class="input date-input__input input--width-2" id="singDate-day" name="singDate-day" type="number" pattern="[0-9]*">
         </div>
       </div>
-      <div class="govuk-date-input__item">
-        <div class="govuk-form-group">
-          <label class="govuk-label govuk-date-input__label" for="dob-month">
+      <div class="date-input__item">
+        <div class="form-group">
+          <label class="label date-input__label" for="singDate-month">
             Month
           </label>
-          <input class="govuk-input govuk-date-input__input govuk-input--width-2" id="dob-month" name="dob-month" type="number" pattern="[0-9]*">
+          <input class="input date-input__input input--width-2" id="singDate-month" name="singDate-month" type="number" pattern="[0-9]*">
         </div>
       </div>
-      <div class="govuk-date-input__item">
-        <div class="govuk-form-group">
-          <label class="govuk-label govuk-date-input__label" for="dob-year">
+      <div class="date-input__item">
+        <div class="form-group">
+          <label class="label date-input__label" for="singDate-year">
             Year
           </label>
-          <input class="govuk-input govuk-date-input__input govuk-input--width-4" id="dob-year" name="dob-year" type="number" pattern="[0-9]*">
+          <input class="input date-input__input input--width-4" id="singDate-year" name="singDate-year" type="number" pattern="[0-9]*">
         </div>
       </div>
     </div>
   </fieldset>
 </div>
-
+<button type="submit" class="btn btn-primary">
+  Submit
+</button>
+</form>
+<?php if(isset($output)) {
+  echo '<p>' . $output . '</p>';
+}?>
+</div>
 </body>
 </html>
