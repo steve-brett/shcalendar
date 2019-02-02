f83d96c847424bcc6c1a2bf47c6851b9b62226d7
Steve <steve@steves-macbook-pro.local>
2019-02-02 13:32:49 +0000

    Validate test case

--- src/Rule.php
+++ src/Rule.php
@@ -14,7 +14,9 @@ class Rule
       ];
     $day = $date->format('d');
     $rule['BYDAY'] = floor($day / 7) + 1 . 'SU';
-
+    if ($date->format('m') == '6') {
+      $rule['BYMONTH'] = '6';
+    }
     return $rule;
   }
 
