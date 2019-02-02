d43738b1c71a1b9c2d59e9be6050d9739cfb9ea2
Steve <steve@steves-macbook-pro.local>
2019-02-02 16:05:00 +0000

    Validate test case

--- src/Rule.php
+++ src/Rule.php
@@ -11,12 +11,17 @@ class Rule
     if ($date < \DateTime::createFromFormat('Y-m-d', '1800-01-01')) {
       throw new \InvalidArgumentException('Date must 1800-01-01 or after. Got [' . $date->format('Y-m-d') .']');
     }
+
     $rule = [
       'OFFSET' => '0'
       ];
-    $day = $date->format('d');
-    $rule['BYDAY'] = floor(($day - 1) / 7) + 1 . 'SU';
-    $rule['BYMONTH'] = $date->format('n');
+    $nextSunday = clone $date;
+    $nextSunday->modify('this sunday');
+
+    $refDay = $nextSunday->format('d');
+    $rule['BYDAY'] = floor(($refDay - 1) / 7) + 1 . 'SU';
+    $rule['BYMONTH'] = $nextSunday->format('n');
+
     if ($rule['BYDAY'] == '5SU') {
       $rule['BYDAY'] = '-SU';
     }
