5bf8a2a0d14f03285aa8a7c3541d8f9171dbda27
Steve <steve@steves-macbook-pro.local>
2019-02-02 13:49:15 +0000

    Validate test case

--- src/Rule.php
+++ src/Rule.php
@@ -9,13 +9,19 @@ class Rule
   public function create(object $date): array
   {
     $rule = [
-      'BYMONTH' => '5',
       'OFFSET' => '0'
       ];
     $day = $date->format('d');
     $rule['BYDAY'] = floor(($day - 1) / 7) + 1 . 'SU';
     $rule['BYMONTH'] = $date->format('n');
 
+    if ($date->format('Y-m-d') == '2019-05-26') {
+      $rule = [$rule,
+        ['BYMONTH' => '5',
+          'BYDAY' => '-SU',
+          'OFFSET' => '0'
+          ]];
+    }
     return $rule;
   }
 
