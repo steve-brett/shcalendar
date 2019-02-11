7af268141883b9fb5bc25f0d428adbac596df661
Steve <steve.brett.design@gmail.com>
2019-02-11 16:59:16 +0000

    Validate exception case

--- src/NthDayRule.php
+++ src/NthDayRule.php
@@ -13,12 +13,18 @@ class NthDayRule implements Rule
     if ($date < \DateTime::createFromFormat('Y-m-d', '1800-01-01')) {
       throw new \InvalidArgumentException('Date must be 1800-01-01 or after. Got [' . $date->format('Y-m-d') .']');
     }
+    
     $refDay = $date->format('d');
+    $count = floor(($refDay - 1) / 7) + 1;
+
+    if ($count == 5) {
+      throw new \InvalidArgumentException('Date is 5th of type in month - not annual. Got [' . $date->format('Y-m-d') .']');
+    }
 
     $day = strtoupper(substr($date->format('D'), 0, -1));
 
     $rule = ['OFFSET' => '0'];
-    $rule['BYDAY'] = floor(($refDay - 1) / 7) + 1 . $day;
+    $rule['BYDAY'] = $count . $day;
     $rule['BYMONTH'] = $date->format('n');
 
     return $rule;
