ac005a1835c952e809a4c4fe4be66a0edcea7b39
Steve <steve.brett.design@gmail.com>
2019-05-22 23:13:08 +0100

    Validate test case and rename some vars

--- src/NthSundayRule.php
+++ src/NthSundayRule.php
@@ -8,25 +8,27 @@ use SHCalendar\Rule;
 class NthSundayRule implements Rule
 {
 
-  public function create(\DateTime $date, string $refday = 'Sun'): array
+  public function create(\DateTime $date, string $refDay = 'Sun'): array
   {
     if ($date < \DateTime::createFromFormat('Y-m-d', '1800-01-01')) {
       throw new \InvalidArgumentException('Date must be 1800-01-01 or after. Got [' . $date->format('Y-m-d') .']');
     }
 
-    $nextSunday = clone $date;
-    $nextSunday->modify('this sun');
-    $refDay = $nextSunday->format('d');
+    $nextRefDay = clone $date;
+    $nextRefDay->modify('this ' . $refDay);
 
-    $count = floor(($refDay - 1) / 7) + 1;
+    // Find position of refDay in month
+    $count = floor(($nextRefDay->format('d') - 1) / 7) + 1;
     if ($count == 5) {
-      throw new \InvalidArgumentException('Date is 5th Sunday in month - not annual. Got [' . $date->format('Y-m-d') .']');
+      throw new \InvalidArgumentException('Date is 5th of type in month - not annual. Got [' . $date->format('Y-m-d') .']');
     }
 
-    $rule['BYDAY'] = $count . 'SU';
-    $rule['BYMONTH'] = $nextSunday->format('n');
-    $rule['OFFSET'] = $nextSunday->diff($date)->format('%R%a');
-    
+    $day = strtoupper(substr($nextRefDay->format('D'), 0, -1));
+
+    $rule['BYDAY'] = $count . $day;
+    $rule['BYMONTH'] = $nextRefDay->format('n');
+    $rule['OFFSET'] = $nextRefDay->diff($date)->format('%R%a');
+
     return $rule;
   }
 
