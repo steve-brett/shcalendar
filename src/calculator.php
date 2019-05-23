70a88fdd09ad7573a3c53a5454adf2563e617885
Steve <steve.brett.design@gmail.com>
2019-05-23 22:53:22 +0100

    REMOVE try-catch as datetime doesn't throw exception

--- src/calculator.php
+++ src/calculator.php
@@ -3,12 +3,22 @@ declare(strict_types=1);
 
 namespace SHCalendar;
 
-$start = $_GET["start"];
-$end = $_GET["end"];
+$start = isset($_GET['start']) ? $_GET['start'] : '';
+  $end = isset($_GET['end']) ? $_GET['end'] : '';
 
-try {
-    $start_object = \DateTime::createFromFormat(\DateTimeInterface::ATOM, $start);
-    echo $start_object->format('D j M Y');
-} catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), PHP_EOL;
+$start_object = \DateTime::createFromFormat(\DateTimeInterface::ATOM, $start);
+  $end_object = \DateTime::createFromFormat(\DateTimeInterface::ATOM, $end);
+
+if (($start_object && $end_object) !== false)
+{
+  echo $start_object->format('D j M Y') . PHP_EOL;
+  echo   $end_object->format('D j M Y') . PHP_EOL;
+  // send dates to function here
 }
+else
+{
+  echo 'Invalid date. ';
+}
+echo 'Got START: ' . $start . ', END: ' . $end . PHP_EOL;
+
+echo '<br><br><a href="http://shcalendar.localhost/src/calculator.php?start=2019-05-23T23%3A14%3A57%2B02%3A00">calculator.php?start=2019-05-23T23%3A14%3A57%2B02%3A00</a>';
