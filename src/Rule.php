139b00beb6df51b5c7c9aeb20b922853c7115377
Steve <steve.brett.design@gmail.com>
2019-05-28 17:07:27 +0100

    Refactor to get day name

--- src/Rule.php
+++ src/Rule.php
@@ -2,33 +2,46 @@
 declare(strict_types=1);
 
 namespace SHCalendar;
+use RRule\RRule;
 
 class Rule
 {
+  /**
+	 * Weekdays numbered from 1 (ISO-8601 or `date('N')`).
+	 * Used internally but public if a reference list is needed.
+	 *
+	 * @todo should probably be protected, with a static getter instead
+	 * to avoid unintended modification
+	 *
+	 * @var array The name as the key
+	 */
+	protected static $week_days = array(
+		'MO' => 'Monday',
+		'TU' => 'Tuesday',
+		'WE' => 'Wednesday',
+		'TH' => 'Thursday',
+		'FR' => 'Friday',
+		'SA' => 'Saturday',
+		'SU' => 'Sunday'
+  );
+
   public function readable(array $rule): string
   {
     $dateObj   = \DateTime::createFromFormat('!m', sprintf("%02s", $rule['BYMONTH']) );
     $monthName = $dateObj->format('F');
 
-    if ($rule['BYDAY'] == '1SA')
-    {
-      return 'The first Saturday in ' . $monthName;
-    }
-    if ($rule['BYDAY'] == '1MO')
-    {
-      return 'The first Monday in ' . $monthName;
-    }
-
+    $dayName = $this::$week_days[substr($rule['BYDAY'], 1, 2)];
+   
     if ($rule['BYDAY'] == '-SU')
     {
-      return 'The last Sunday in ' . $monthName;
+      return 'The last ' . $dayName . ' in ' . $monthName;
     }
     $ordinal = substr($rule['BYDAY'], 0, 1);
 
     $formatter = new \NumberFormatter('en_US', \NumberFormatter::SPELLOUT);
     $formatter->setTextAttribute(\NumberFormatter::DEFAULT_RULESET, "%spellout-ordinal");
 
-    return 'The ' . $formatter->format($ordinal) . ' Sunday in ' . $monthName;
+    return 'The ' . $formatter->format($ordinal) . ' ' . $dayName . ' in ' . $monthName;
   }
 
 }
