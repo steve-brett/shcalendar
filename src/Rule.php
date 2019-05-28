a24319421038fc9d9e0c30b56212e51d6f432d31
Steve <steve.brett.design@gmail.com>
2019-05-28 15:04:09 +0100

    Validate test case

--- src/Rule.php
+++ src/Rule.php
@@ -7,6 +7,10 @@ class Rule
 {
   public function readable(array $rule): string
   {
+    if ($rule['BYDAY'] == '3SU')
+    {
+      return 'The third Sunday in May';
+    }
     if ($rule['BYDAY'] == '2SU')
     {
       return 'The second Sunday in May';
