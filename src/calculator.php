0b71c952d2fb1681ab258909dd739ce599f30fe9
Steve <steve.brett.design@gmail.com>
2019-06-11 17:21:06 +0100

    ADD radio buttons

--- src/calculator.php
+++ src/calculator.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 namespace SHCalendar;
+
 include 'dateFormula.php';
 use SHCalendar\singingFormula;
 use SHCalendar\interpretFormula;
@@ -22,10 +23,34 @@ if ( ($start_object == false) || ($end_object == false) )
   exit();
 }
 
-$formulae = new singingFormula($start_object);
-$formulae = $formulae->createFormulae();
-echo $start_object->format('l jS F Y') . PHP_EOL;
-echo   $end_object->format('l jS F Y') . PHP_EOL;
+
 // send dates to function here
+?>
+
+<?php $formulae = new singingFormula($start_object); ?>
+<?php $formulae = $formulae->createFormulae(); ?>
+
+<tr>
+  <td></td>
+  <td>
+    <span>Choose the date formula that describes your singing.<br/>
+    If you are not presented with a correct formula, please <a href="mailto:steve.brett.design@gmail.com">email me</a>.
+    </span>
+  </td>
+</tr>
+<?php foreach ($formulae as $k => $formula) {
+    ?>
+    <tr>
+  <td></td>
+  <td>
+  <input class="govuk-radios__input" id="dateFormula-<?php echo $k; ?>" name="dateFormula" type="radio" value="<?php print_r(implode(",", $formula))?>">
+  <label class="govuk-label govuk-radios__label" for="dateFormula-<?php echo $k; ?>">
+    <?php   $output = new interpretFormula($formula, $start_object->format('Y'));
+    echo $output->text() . PHP_EOL; ?>
+  </label>
+  </td>
+</tr>
+<?php
+} ?>
 
 
