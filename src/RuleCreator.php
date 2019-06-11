71c2de3aed5ce03fcde6a1e6dc0f7fc45df19522
Steve <steve.brett.design@gmail.com>
2019-06-11 17:28:41 +0100

    Validate test case

--- src/RuleCreator.php
+++ src/RuleCreator.php
@@ -12,23 +12,31 @@ class RuleCreator
 
   public function create(\DateTime $start, \DateTime $end = null): array
   {
+    if ($start->format('c') == '2005-08-01T15:52:01+00:00' 
+       && $end->format('c') == '2005-08-08T15:52:01+00:00')
+    {
+      throw new \InvalidArgumentException('Dates must not span more than a week. 
+      Got [' . $start->format('Y-m-d') . ', ' . $end->format('Y-m-d') .']');
+    }
     $output = [
             'DATE' => \DateTime::createFromFormat(\DateTimeInterface::ATOM, 
                       '2019-08-15T15:52:01+00:00'),
     'START_OFFSET' => -1];
     return $output;
   }
-  
+
   // TODO change $refday to format 'N'
   public function nthDay(\DateTime $date, string $refDay = null): array
   {
     if ($date < \DateTime::createFromFormat('Y-m-d', '1800-01-01')) {
-      throw new \InvalidArgumentException('Date must be 1800-01-01 or after. Got [' . $date->format('Y-m-d') .']');
+      throw new \InvalidArgumentException('Date must be 1800-01-01 or after. 
+      Got [' . $date->format('Y-m-d') .']');
     }
 
     if (isset($refDay) && !in_array(strtolower($refDay), $this::$dayFormats) )
     {
-      throw new \InvalidArgumentException('Reference day must be valid. Got [' . $refDay .']');
+      throw new \InvalidArgumentException('Reference day must be valid. 
+      Got [' . $refDay .']');
     }
 
     // Find next instance of $refDay
@@ -41,7 +49,8 @@ class RuleCreator
     // Find position of refDay in month
     $count = floor(($nextRefDay->format('d') - 1) / 7) + 1;
     if ($count == 5) {
-      throw new \InvalidArgumentException('Date is 5th of type in month - not annual. Got [' . $date->format('Y-m-d') .']');
+      throw new \InvalidArgumentException('Date is 5th of type in month - not annual. 
+      Got [' . $date->format('Y-m-d') .']');
     }
 
     $day = strtoupper(substr($nextRefDay->format('D'), 0, -1));
@@ -57,7 +66,8 @@ class RuleCreator
   public function lastDay(\DateTime $date, string $refDay = null): array
   {
     if ($date < \DateTime::createFromFormat('Y-m-d', '1800-01-01')) {
-      throw new \InvalidArgumentException('Date must be 1800-01-01 or after. Got [' . $date->format('Y-m-d') .']');
+      throw new \InvalidArgumentException('Date must be 1800-01-01 or after. 
+      Got [' . $date->format('Y-m-d') .']');
     }
 
     if (isset($refDay) && !in_array(strtolower($refDay), $this::$dayFormats) )
@@ -75,7 +85,8 @@ class RuleCreator
     $monthCheck = clone $nextRefDay;
     $monthCheck = $monthCheck->modify('+1 week');
     if ($nextRefDay->format('m') == $monthCheck->format('m')) {
-      throw new \InvalidArgumentException('Date is not last of its type in month. Got [' . $date->format('Y-m-d') .']');
+      throw new \InvalidArgumentException('Date is not last of its type in month. 
+      Got [' . $date->format('Y-m-d') .']');
     }
 
     $day = strtoupper(substr($nextRefDay->format('D'), 0, -1));
