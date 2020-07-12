a0685114a43e4c542b9824b292efe9f801e485d0
Steve <steve.brett.design@gmail.com>
2020-07-12 13:58:12 +0100

    Refactor offset to 1SU format

--- src/Rule.php
+++ src/Rule.php
@@ -115,42 +115,42 @@ class Rule
 			throw $e;
 		}
 
-		if ($rule['OFFSET'] && $rule['OFFSET'] !== 0) {
-			
-			$offset = $rule['OFFSET'];
-
-			if ($rule['BYMONTH'] > 2) {
-				// Affected by leap year
-				$by_month_day = 'BYMONTHDAY=';
-				for ($k = 0 ; $k < 7; $k++) { 
-					$by_month_day .= $this->year_day(1 + $offset, $k) . ','; 
-				}
-				$by_month_day = substr($by_month_day, 0, -1) . ';';
-				$year_day_limit = 8;
-			} else {
-				// Unaffected by leap year
-				$by_month_day = '';
-				$year_day_limit = 7;
-			}
-
-			$year_day = $this::$first_of_month[$rule['BYMONTH']] + $offset;
-			
-			$by_year_day = 'BYYEARDAY=';
-			for ($k = 0 ; $k < $year_day_limit; $k++) { 
-				$by_year_day .= $this->year_day($year_day, $k) . ','; 
-			}
-			$by_year_day = substr($by_year_day, 0, -1);
-
-			if ($offset < 0) {
-				$by_day = array_reverse(array_keys($this::$week_day_abbrev))[abs($offset)];
-			} else {
-				$by_day = array_keys($this::$week_day_abbrev)[$offset - 1];
-			}
-			$by_day = 'BYDAY=' . $by_day . ';';
-
-			return 'FREQ=YEARLY;INTERVAL=1;'. $by_day . $by_month_day . $by_year_day;
+		if ( !isset( $rule['OFFSET'] ) ){
+			return 'FREQ=YEARLY;INTERVAL=1;BYMONTH='. $rule['BYMONTH'] . ';BYDAY=' . $rule['BYDAY'];
 		}
-		return 'FREQ=YEARLY;INTERVAL=1;BYMONTH='. $rule['BYMONTH'] . ';BYDAY=' . $rule['BYDAY'];
+			
+		$offset = $this->calculate_offset_days( substr($rule['BYDAY'], -2), $rule['OFFSET'] );
+
+		if ($rule['BYMONTH'] > 2) {
+			// Affected by leap year
+			$by_month_day = 'BYMONTHDAY=';
+			for ($k = 0 ; $k < 7; $k++) { 
+				$by_month_day .= $this->year_day(1 + $offset, $k) . ','; 
+			}
+			$by_month_day = substr($by_month_day, 0, -1) . ';';
+			$year_day_limit = 8;
+		} else {
+			// Unaffected by leap year
+			$by_month_day = '';
+			$year_day_limit = 7;
+		}
+
+		$year_day = $this::$first_of_month[$rule['BYMONTH']] + $offset;
+		
+		$by_year_day = 'BYYEARDAY=';
+		for ($k = 0 ; $k < $year_day_limit; $k++) { 
+			$by_year_day .= $this->year_day($year_day, $k) . ','; 
+		}
+		$by_year_day = substr($by_year_day, 0, -1);
+
+		if ($offset < 0) {
+			$by_day = array_reverse(array_keys($this::$week_day_abbrev))[abs($offset)];
+		} else {
+			$by_day = array_keys($this::$week_day_abbrev)[$offset - 1];
+		}
+		$by_day = 'BYDAY=' . $by_day . ';';
+
+		return 'FREQ=YEARLY;INTERVAL=1;'. $by_day . $by_month_day . $by_year_day;
 	}
 	
 	/**
@@ -178,12 +178,9 @@ class Rule
 
 		$dayName = $this::$week_day_abbrev[substr($rule['BYDAY'], -2)];
 
-		 if ($rule['OFFSET'] !== 0)
+		if ( isset( $rule['OFFSET'] ) )
 		{
-			$weekDay = $rule['OFFSET'] + \RRule\RRule::$week_days[substr($rule['BYDAY'], -2)];
-			$weekDay = ($weekDay + 7) % 7;
-
-			$offset = $this::$week_days[$weekDay] . ' before the ';
+			$offset = $this::$week_day_abbrev[substr($rule['OFFSET'], -2)] . ' before the ';
 		}
 
 		$ordinal = substr($rule['BYDAY'], 0, -2);
@@ -207,17 +204,13 @@ class Rule
 	 */
 	protected function validate (array $rule): array
 	{
-		if (!isset($rule['OFFSET']) )
-		{
-			$rule['OFFSET'] = 0;
-		}
-		if ( is_int($rule['OFFSET']) == false )
+		// if (!isset($rule['OFFSET']) )
+		// {
+		// 	$rule['OFFSET'] = 0;
+		// }
+		if ( isset( $rule['OFFSET'] ) && !$this->valid_offset( $rule['OFFSET'] ) )
 		{
 			throw new \InvalidArgumentException('OFFSET format incorrect. Got [' . $rule['OFFSET'] . ']');
-		}
-		if (abs($rule['OFFSET']) >= 7 )
-		{
-			throw new \InvalidArgumentException('OFFSET must be between -6 and 6. Got [' . $rule['OFFSET'] . ']');
 		}
 
 		if ( isset($rule['SPECIAL']) )
@@ -261,6 +254,61 @@ class Rule
 		}
 
 		return $rule;
+	}
+
+	/**
+	 * Is 'OFFSET' valid?
+	 *
+	 * @param mixed $offset
+	 * @return boolean
+	 */
+	protected function valid_offset( $offset ) : bool
+	{	
+		if ( !is_string($offset) ) {
+			return false;
+		}
+
+		// String is 3-4 chars long
+		if ( strlen($offset) > 4 || strlen($offset) < 3 ) {
+			return false;
+		}
+
+		// Last two chars are weekdays
+		if ( !array_key_exists( substr($offset, -2), $this::$week_day_abbrev) )
+		{
+			return false;
+		}
+
+		// Prefix is 1 or -1
+		if ( abs( (int) substr($offset, 0, -2) ) !== 1) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Calculate number of days between a day of the week and its offset.
+	 * 
+	 * @example calculate_offset('SU','-1SA') => -1
+	 *
+	 * @param string $day MO,TU,WE,TH,FR,SA,SU
+	 * @param string $offset 1MO,1TU,... or -1MO,-1TU,...
+	 * @return integer
+	 */
+	public function calculate_offset_days ( string $day, string $offset ) : int
+	{
+		$day = \RRule\RRule::$week_days[$day];
+		$offset_sign = (int) substr($offset, 0, -2);
+		$offset = $offset_sign * \RRule\RRule::$week_days[substr($offset, -2)];
+
+		// I can only explain this maths by diagram!
+		if ( ($offset - $offset_sign * $day) > 0 ) 
+		{
+			return ($offset_sign * $offset) - $day;
+		}
+
+		return $offset_sign * ($offset + 7) - $day;
 	}
 
 }
